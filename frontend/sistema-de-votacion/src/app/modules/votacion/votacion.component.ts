3e69545c669fd895f5c7964ef6bac8b8b186e74b
Gaston Favre Salas <gastonfavresalas@gmail.com>
2022-10-11 12:22:08 -0300

    initial files

---
+++ frontend/sistema-de-votacion/src/app/modules/votacion/votacion.component.ts
@@ -0,0 +1,24 @@
+import { Web3Service } from 'src/app/services/contract/web3.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-votacion',
+  templateUrl: './votacion.component.html',
+  styleUrls: ['./votacion.component.css']
+})
+export class VotacionComponent implements OnInit {
+
+
+  constructor(private web3Service: Web3Service) { }
+
+  async ngOnInit() {
+  }
+
+  public async conectarWallet() {
+    if (typeof window.ethereum !== 'undefined') {
+      const result = await this.web3Service.connectAccount();
+      console.log(result)
+    }
+  }
+
+}
---
+++ frontend/sistema-de-votacion/src/app/services/contract/web3.service.ts
@@ -0,0 +1,123 @@
+
+import {Injectable} from '@angular/core';
+import { Subject } from 'rxjs';
+import Web3 from 'web3';
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { provider } from 'web3-core';
+//import * as contractABI from '../../../../../../smart-contract/build/contracts/Election.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Web3Service {
+  public accountsObservable = new Subject<string[]>();
+  web3Modal;
+  web3js:  any;
+  provider: provider | undefined | any;
+  accounts: string[] | undefined;
+  balance: string | undefined;
+
+  constructor() {
+    const providerOptions = {
+      walletconnect: {
+        package: WalletConnectProvider, // required | aqui importamos el paquete que nos ayudara a usar soportar distintas wallets
+        options: {
+          infuraId: 'env', // cambia esto con tu apikey de infura
+          description: 'Scan the qr code and sign in', // Puedes camnbiar los textos descriptivos en la seccion description
+          qrcodeModalOptions: {
+            mobileLinks: [
+              'rainbow',
+              'metamask',
+              'argent',
+              'trust',
+              'imtoken',
+              'pillar'
+            ]
+          }
+        }
+      },
+      injected: {
+        display: {
+          logo: 'https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg',
+          name: 'metamask',
+          description: "Connect with the provider in your Browser"
+        },
+        package: null
+      },
+    };
+
+    this.web3Modal = new Web3Modal({
+      network: "Ganache", // puedes cambiar a una red de pruebas o etc
+      cacheProvider: true, // optional
+      providerOptions, // required
+      theme: {
+        background: "rgb(39, 49, 56)",
+        main: "rgb(199, 199, 199)",
+        secondary: "rgb(136, 136, 136)",
+        border: "rgba(195, 195, 195, 0.14)",
+        hover: "rgb(16, 26, 32)"
+      }
+    });
+  }
+
+  public async connectAccount(): Promise<provider | undefined> {
+    try {
+      this.provider = await this.web3Modal.connect(); // set provider
+      if (this.provider) {
+        this.web3js = new Web3(this.provider);
+      } // create web3 instance
+    } catch (error) {
+      console.log(error)
+    }
+    return this.web3js;
+  }
+
+  public async connectContract(): Promise<any>{
+    throw new Error('Method not implemented.');
+  }
+
+  public async payFee(direccionWallet: string): Promise<string | null>{
+    throw new Error('Method not implemented.');
+  }
+
+  public async agregarCandidato(direccionWallet: string, nombre: string, contractOwner: string): Promise<any>{
+    throw new Error('Method not implemented.');
+  }
+
+  public async getContractOwner(): Promise<any>{
+    throw new Error('Method not implemented.');
+  }
+
+  public async consultarCantidadDeCandidatos(){
+    throw new Error('Method not implemented.');
+  }
+
+
+  public async registrarVotante(direccionWallet: string, contractOwner: string): Promise<any>{
+    throw new Error('Method not implemented.');
+  }
+
+
+  public async iniciarVotacion(owner: string): Promise<void>{
+    throw new Error('Method not implemented.');
+  }
+
+  public async finalizarVotacion(owner: string): Promise<void>{
+    throw new Error('Method not implemented.');
+  }
+
+  public async consultarEstadoVotacion(): Promise<any>{
+    throw new Error('Method not implemented.');
+  }
+
+  public async anunciarGanador(owner: string): Promise<any>{
+    throw new Error('Method not implemented.');
+  }
+
+  public async votar(direccionCandidato: string, direccionVotante: string): Promise<any>{
+    throw new Error('Method not implemented.');
+  }
+
+
+}
